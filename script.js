92edd7c4dd6ac3337dcd5e35a15412bb86afb9b3
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-20 16:46:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
+  function cleanInputString(str) {
   const regex = /[+\-\s]/g;
+  console.log("original string: ", str);
+  return str.replace(regex, '');
+}
+  const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
