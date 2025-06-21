159d3309f4ffd8d36516947b334ba5c3b557aedf
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-21 11:52:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetId = `#${entryDropdown.value}`;
+  const targetInputContainer = document.querySelector(`${targetId} .input-container`);
 }
