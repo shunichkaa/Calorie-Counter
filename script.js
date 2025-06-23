8ad4b472828ad7c6d2e5bc4b1ba4c6a4998ed901
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-23 15:30:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
 }
 
 function getCaloriesFromInputs(list) {
