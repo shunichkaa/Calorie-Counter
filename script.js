729d15f91b1032d7c9c08ab59961b8bd00f04d7e
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-25 19:17:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,11 +36,13 @@ function calculateCalories(e) {
   e.preventDefault();
   isError = false;
 
-  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
-  const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
-const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
-const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
-const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+ const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+if (isError) return;
 
 }
 
