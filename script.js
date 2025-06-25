d006272375c2d17bdc4a2054b3d594d4f8b4bcc9
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-25 19:19:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 
 }
 
