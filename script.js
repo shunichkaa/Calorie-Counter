2ba8e47df5f972625e449522009490e19e2115c3
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-27 13:18:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ function clearForm() {
   for (const container of inputContainers) {
     container.innerHTML = '';
   }
+  budgetNumberInput.value = '';
 }
 
 addEntryButton.addEventListener("click", addEntry);
